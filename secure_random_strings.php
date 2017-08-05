c894282a3c2e4c74c252a1a21848915da38ae874
Ivan Matozan <ivan.matozan@gmail.com>
2017-08-05 12:30:23 +0200

    Update Secure random strings

--- secure_random_strings.php
+++ secure_random_strings.php
@@ -3,7 +3,10 @@
 require __DIR__ . '/vendor/autoload.php';
 
 echo bin2hex(openssl_random_pseudo_bytes(128));
+
+// PHP 7
 echo bin2hex(random_bytes(128));
+echo random_int(1, 52);
 
 // RandomLib
 $randomLib = new \RandomLib\Factory();
